da1108423c792b2a2eab41880aedeb059c1c4e3d
pinkxpink <kyperu07@gmail.com>
2025-03-25 12:46:03 +0200

    111

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-CONST_NAME = "Привет это небольшая программа для теста"
-print(CONST_NAME)
+# CONST_NAME = "Привет это небольшая программа для теста"
+# print(CONST_NAME)
+#
+# userName = input("input ur name: ")
+# welcomeMessage = f"Hello {userName}"
+# print (welcomeMessage)
+import sys
 
-userName = input("input ur name: ")
-welcomeMessage = f"Hello {userName}"
-print (welcomeMessage)
+try:
+    userVar = int(input("Введите число: "))
+
+    if userVar <= 55:
+        print(True)
+        print("End")
+    elif userVar <= 100:
+        print("Digits <= 100")
+
+
+except ValueError:
+    print("Ошибка: введено не число!", file=sys.stderr)
