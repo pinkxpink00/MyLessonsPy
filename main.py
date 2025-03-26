4c25872f38f492116acb5ce0f6e26234b55eb430
pinkxpink <kyperu07@gmail.com>
2025-03-26 21:22:16 +0200

    111

--- main.py
+++ main.py
@@ -4,17 +4,45 @@
 # userName = input("input ur name: ")
 # welcomeMessage = f"Hello {userName}"
 # print (welcomeMessage)
-import sys
+# import sys
+#
+# try:
+#     userVar = int(input("Введите число: "))
+#
+#     if userVar <= 55:
+#         print(True)
+#         print("End")
+#     elif userVar <= 100:
+#         print("Digits <= 100")
+#
+#
+# except ValueError:
+#     print("Ошибка: введено не число!", file=sys.stderr)
 
-try:
-    userVar = int(input("Введите число: "))
+# for i in range(11):
+#     print("Element",i)
 
-    if userVar <= 55:
-        print(True)
-        print("End")
-    elif userVar <= 100:
-        print("Digits <= 100")
+# for i in range (5,16,3):
+#     print("Element", i)
 
+# word = 'some text'
+#
+# for i in word:
+#     if i == 't':
+#       print("t has been")
 
-except ValueError:
-    print("Ошибка: введено не число!", file=sys.stderr)
+# i = 0
+# while i <= 10:
+#     print(i)
+#     i += 1
+
+# work = True
+# while work:
+#     user_input = input("Enter word STOP:")
+#     if user_input == "STOP":
+#         work = False
+#         break
+# else:
+#     for i in range(2):
+#         print(i)
+
